a94b7207f86945776b71b86eebe2e72dce783065
Mario sanchez <mariojose1693@gmail.com>
2017-04-27 10:14:57 -0600

    ScrollView programable

--- Platzigram/ProfileViewController.swift
+++ Platzigram/ProfileViewController.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 class ProfeliViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
     
     @IBAction func presentarNuevaVista(_ sender: Any) {
         self.performSegue(withIdentifier: "nuevaVista", sender: nil)
@@ -17,6 +19,38 @@ class ProfeliViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0...5 {
+            let newView = UIView()
+            newView.frame = CGRect(x: CGFloat(i) * self.view.frame.size.width, y: 0, width: scrollView.frame.size.width, height: self.view.frame.size.height)
+            
+            newView.backgroundColor = UIColor(red: CGFloat(arc4random_uniform(255))/255.0, green: CGFloat(arc4random_uniform(255))/255.0, blue: CGFloat(arc4random_uniform(255))/255.0, alpha: 1.0)
+            
+            scrollView.addSubview(newView)
+        }
+        // agregamos el contenedor del scrollView
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width * 5, height: self.scrollView.frame.size.height)
+        scrollView.showsHorizontalScrollIndicator = false
     }
     
+    @IBAction func movimientoPresionado(_ sender: UIButton) {
+        // punto con respecto a la pantalla
+        let currentX = self.scrollView.contentOffset.x
+        let vistaActual = Int(currentX / scrollView.frame.size.width)
+        var nuevaVista = 0
+        if  (sender.title(for: .normal)! == "Atras"){
+            nuevaVista = vistaActual - 1;
+            
+        } else {
+            nuevaVista = vistaActual + 1
+        }
+        // para tomar en cuenta que no se salga del content del scrollview
+        if(nuevaVista<0 || nuevaVista > 5){
+            return
+        }
+        
+        let newX = CGFloat(nuevaVista) * self.scrollView.frame.size.width
+        scrollView.contentOffset = CGPoint(x: newX, y: scrollView.contentOffset.y)
+    }
+ 
 }
