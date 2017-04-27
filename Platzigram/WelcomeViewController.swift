95aa55de4051c7f4670d2a85324ad72652ecdf18
Mario sanchez <mariojose1693@gmail.com>
2017-04-27 02:19:12 -0600

    UIActionAlert, .alert & .actionSheet

--- Platzigram/WelcomeViewController.swift
+++ Platzigram/WelcomeViewController.swift
@@ -34,13 +34,98 @@ class WelcomeViewController: UIViewController {
         botonSignup?.setTitleColor(UIColor.red, for: .normal)
         botonSignup?.sizeToFit()
         botonSignup?.frame = CGRect(x: 20, y: self.view.frame.size.height - 15 - 20, width: self.view.frame.size.width - 40, height: 20)
-        botonSignup?.addTarget(self, action: #selector(signupPressed(sender:)), for: .touchUpInside)
+        botonSignup?.addTarget(self, action: #selector(apretoBoton(sender:)), for: .touchUpInside)
         self.view.addSubview(botonSignup!)
         
     }
     
-    func signupPressed(sender: UIButton) {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if etiquetaBienvenida == nil{
+            etiquetaBienvenida = UILabel()
+            etiquetaBienvenida?.text = "Bienvenido a platzigram"
+            etiquetaBienvenida?.font = UIFont.systemFont(ofSize: 17)
+            etiquetaBienvenida?.textColor = UIColor.orange
+            etiquetaBienvenida?.sizeToFit()
+            etiquetaBienvenida?.frame = CGRect(x: 0, y: 20, width: etiquetaBienvenida!.frame.size.width, height: etiquetaBienvenida!.frame.size.height)
+            etiquetaBienvenida?.center = CGPoint(x: self.view.frame.size.width / 2, y: etiquetaBienvenida!.center.y)
+            
+            self.view.addSubview(etiquetaBienvenida!)
+        }
+        
+        if botonSignup == nil {
+            botonSignup = UIButton(type: .custom)
+            botonSignup?.setTitle("Signup", for: .normal)
+            botonSignup?.setTitleColor(UIColor.red, for: .normal)
+            botonSignup?.sizeToFit()
+            botonSignup?.addTarget(self, action: #selector(apretoBoton(sender:)), for: .touchUpInside)
+            botonSignup?.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(botonSignup!)
+            
+            self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-20-[botonSignup]-20-|", options: NSLayoutFormatOptions(rawValue:0), metrics: nil, views: ["botonSignup":botonSignup!]))
+            self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[botonSignup]-space-|", options: NSLayoutFormatOptions(rawValue:0), metrics: ["space":20], views: ["botonSignup":botonSignup!]))
+            self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[botonSignup(==40)]", options: NSLayoutFormatOptions(rawValue:0), metrics: nil, views: ["botonSignup":botonSignup!]))
+        }
+        
+        if botonLogin == nil {
+            botonLogin = UIButton(type: .custom)
+            botonLogin?.setTitle("Login", for: .normal)
+            botonLogin?.setTitleColor(UIColor.blue, for: .normal)
+            botonLogin?.sizeToFit()
+            botonLogin?.frame = CGRect(x: 20, y: botonSignup!.frame.origin.y - 20 - 20, width: self.view.frame.size.width - 40, height: 20)
+            botonLogin?.addTarget(self, action: #selector(apretoBoton(sender:)), for: .touchUpInside)
+            self.view.addSubview(botonLogin!)
+            
+        }
+        
+        if imageBienvenida == nil {
+            imageBienvenida = UIImageView(image: UIImage(named: "PlatziLogo"))
+            imageBienvenida?.frame = CGRect(x: 20, y: imageBienvenida!.frame.maxY + 40, width: self.view.frame.size.width - 40, height: 80)
+            imageBienvenida?.contentMode = .scaleAspectFit
+            self.view.addSubview(imageBienvenida!)
+        }
+    }
+    
+    
+    func apretoBoton(sender: UIButton) {
         print("Presiono \(sender.title(for: .normal)!)")
+        /*
+         let alerta = UIAlertController(title: "Sin cuenta", message: "Por el momento no tienes una cuenta", preferredStyle: .actionSheet)
+         let crearCuenta = UIAlertAction(title: "Crear cuenta", style: .default, handler: {
+         actionCrearCuenta in
+         print ("Presiono crear cuenta")
+         })
+         let cancelar = UIAlertAction(title: "Cancelar ", style:.cancel, handler:{
+         accionCancelar in
+         print("Presiono el boto cancelar")
+         
+         })
+         
+         alerta.addAction(cancelar)
+         alerta.addAction(crearCuenta)
+         
+         self.present(alerta, animated: true, completion: nil)*/
+        let alerta = UIAlertController(title: "Sin cuenta", message: "Por el momento no tienes una cuenta", preferredStyle: .actionSheet)
+        let crearCuenta = UIAlertAction(title: "Crear cueta", style: .default, handler: {
+            actionCrearCuenta in
+            print("Craer Cuenta")
+        })
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: {
+            actionCancelar in
+            print("Presionaste Cancelar")
+        })
+        
+        alerta.addAction(cancelar)
+        alerta.addAction(crearCuenta)
+        
+        // como presentare mi alert
+        self.present(alerta,animated: true,completion: nil)
+//        
+//        if sender .isEqual(botonSignup!){
+//            self.performSegue(withIdentifier: "signup", sender: nil)
+//        }
     }
     
     
