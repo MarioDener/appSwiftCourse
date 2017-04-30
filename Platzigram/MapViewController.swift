87f6615a6511132411a11ca36b7e41cd5a37f820
Mario sanchez <mariojose1693@gmail.com>
2017-04-29 21:31:15 -0600

    corelocation region direcciones

--- Platzigram/MapViewController.swift
+++ Platzigram/MapViewController.swift
@@ -14,6 +14,8 @@ import CoreLocation
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var centrarButton: UIButton!
+    @IBOutlet weak var etiquetaDistancia: UILabel!
+    @IBOutlet weak var etiquetaDireccion: UILabel!
     let locationManager = CLLocationManager()
     let coordinates = [(4.6948,-74808),(4.7313,-74.0660),(4.7016,-74.0974),(4.726123,-74.0360)]
     
@@ -56,14 +58,34 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         let latitude  = mapView.centerCoordinate.latitude
         let longitude = mapView.centerCoordinate.longitude
-        
         print("Latitud :\(latitude) Longitud :\(longitude)")
-        
         if ((latitude != mapView.userLocation.coordinate.latitude) && (longitude != mapView.userLocation.coordinate.longitude)) {
             centrarButton.isHidden = false
         }
+        let locationCoordinate = CLLocation(latitude: latitude, longitude: longitude)
+        let distanceMeters = mapView.userLocation.location?.distance(from: locationCoordinate)
         
+        if (distanceMeters != nil) {
+            etiquetaDistancia.text = "Distancia entre usuario : \(distanceMeters!)"
+            self.geocode(latitud:latitude,longitud:longitude)
+        } else {
+            etiquetaDistancia.text = "No se puede calcular la distancia"
+        }        
+    }
+    
+    func geocode(latitud: CLLocationDegrees, longitud: CLLocationDegrees) {
+        let geocoder = CLGeocoder()
+        let location = CLLocation(latitude:latitud,longitude:longitud)
         
+        geocoder.reverseGeocodeLocation(location,completionHandler: {
+            (placemarks, error) in 
+            if placemarks != nil{
+                let placemarks = placemarks?.first!
+                self.etiquetaDireccion.text = "\(placemarks?.name!, placemarks?.country!)"
+            } else {
+                self.etiquetaDireccion.text = "No se pudo obtener la direccion"
+            }
+        })
     }
     
 }
